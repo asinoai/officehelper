7e0fe5206ea4ebdd6af30773a84dfa95f74ae0c0
aronsinoai_cp <SND\aronsinoai_cp>
2011-04-07 19:37:25 +0000

    Adding sample application; exporting tables from an sqlite database

---
+++ sqliteexporter/MainForm.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.goButton = new System.Windows.Forms.Button();
+            this.tableNameComboBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dbFileTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dbBrowseButton = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // goButton
+            // 
+            this.goButton.Location = new System.Drawing.Point(346, 27);
+            this.goButton.Name = "goButton";
+            this.goButton.Size = new System.Drawing.Size(96, 23);
+            this.goButton.TabIndex = 0;
+            this.goButton.Text = "&Export";
+            this.goButton.UseVisualStyleBackColor = true;
+            this.goButton.Click += new System.EventHandler(this.goButton_Click);
+            // 
+            // tableNameComboBox
+            // 
+            this.tableNameComboBox.FormattingEnabled = true;
+            this.tableNameComboBox.Location = new System.Drawing.Point(12, 79);
+            this.tableNameComboBox.Name = "tableNameComboBox";
+            this.tableNameComboBox.Size = new System.Drawing.Size(254, 21);
+            this.tableNameComboBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Table name:";
+            // 
+            // dbFileTextBox
+            // 
+            this.dbFileTextBox.Location = new System.Drawing.Point(12, 27);
+            this.dbFileTextBox.Name = "dbFileTextBox";
+            this.dbFileTextBox.Size = new System.Drawing.Size(254, 20);
+            this.dbFileTextBox.TabIndex = 3;
+            this.dbFileTextBox.TextChanged += new System.EventHandler(this.dbFileTextBox_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Db file:";
+            // 
+            // dbBrowseButton
+            // 
+            this.dbBrowseButton.Location = new System.Drawing.Point(273, 27);
+            this.dbBrowseButton.Name = "dbBrowseButton";
+            this.dbBrowseButton.Size = new System.Drawing.Size(26, 20);
+            this.dbBrowseButton.TabIndex = 5;
+            this.dbBrowseButton.Text = "..";
+            this.dbBrowseButton.UseVisualStyleBackColor = true;
+            this.dbBrowseButton.Click += new System.EventHandler(this.dbBrowseButton_Click);
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(467, 222);
+            this.Controls.Add(this.dbBrowseButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dbFileTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tableNameComboBox);
+            this.Controls.Add(this.goButton);
+            this.Name = "mainForm";
+            this.Text = "SqliteExporter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button goButton;
+        private System.Windows.Forms.ComboBox tableNameComboBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox dbFileTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button dbBrowseButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+    }
+}
+
---
+++ sqliteexporter/MainForm.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml.Linq;
+using System.Data.SQLite;
+using Aron.Sinoai.OfficeHelper;
+
+namespace WindowsFormsApplication3
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void goButton_Click(object sender, EventArgs e)
+        {
+            export(tableNameComboBox.Text);
+        }
+
+        private void dbBrowseButton_Click(object sender, EventArgs e)
+        {
+            openFileDialog.FileName = dbFileTextBox.Text;
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                dbFileTextBox.Text = openFileDialog.FileName;
+            }
+
+        }
+
+        private void dbFileTextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (File.Exists(dbFileTextBox.Text))
+            {
+                refreshTableNames();
+            }
+        }
+
+        private SQLiteConnection createConnection()
+        {
+            SQLiteConnection connection = new SQLiteConnection(string.Format("Data Source={0};FailIfMissing=true", dbFileTextBox.Text));
+            connection.Open();
+            return connection;
+        }
+
+        private List<string> fetchTables(SQLiteConnection connection)
+        {
+            DataTable columns = connection.GetSchema("Tables");
+
+            IEnumerable<string> result =
+                from row in columns.Select()
+                select row.Field<String>("TABLE_NAME");
+
+            return result.ToList<string>();
+        }
+
+        private List<string> fetchColumns(SQLiteConnection connection, string tableName)
+        {
+            DataTable columns = connection.GetSchema("Columns");
+
+            IEnumerable<string> result =
+                from row in columns.Select(String.Format("TABLE_NAME = '{0}'", tableName))
+                select row.Field<String>("COLUMN_NAME");
+
+            return result.ToList<string>();
+        }
+
+        private IEnumerable<List<object>> fetchData(SQLiteConnection connection, string tableName, List<string> columns)
+        {
+            string separator = "";
+            StringBuilder buffer = new StringBuilder();
+            foreach(string column in columns)
+            {
+                buffer.Append(separator);
+                buffer.Append(column);
+
+                if (separator.Length == 0) 
+                {
+                    separator = ", ";
+                }
+            }
+
+            int count = columns.Count;
+            List<object> result = new List<object>(count);
+            
+            SQLiteCommand command = connection.CreateCommand();
+            command.CommandText = string.Format("select {0} from {1}", buffer.ToString(), tableName);
+            using (SQLiteDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    result.Clear();
+                    for(int i = 0; i < count; i++)
+                    {
+                        object value = reader.GetValue(i);
+                        if (value is string)
+                        {
+                            string stringValue = (string)value;
+                            if (stringValue.Contains((char)0x1a))
+                            {
+                                stringValue = stringValue.Replace((char)0x1a, ' ');
+                                value = stringValue;
+                            }
+                            else
+                            {
+                                value = stringValue;
+                            }
+                        }
+                             
+                        result.Add(value);
+
+
+                    }
+                    
+                    yield return result;
+                }
+            }
+        }
+
+        /*private void init()
+        {
+            using (TextReader patchContentReader = new StreamReader(@"resources\database.xml") )
+            {
+                XElement root = XElement.Load(patchContentReader);
+
+                var result =
+                    from item in root.Element("tables").Elements("item")
+                    select item.Attribute("name").Value;
+
+                tableNameComboBox.DataSource = result.ToList<String>();
+            }
+        }*/
+
+        private void refreshTableNames()
+        {
+            SQLiteConnection connection = createConnection();
+            using (connection)
+            {
+                List<string> tables = fetchTables(connection);
+                tables.Sort();
+                tableNameComboBox.DataSource = tables;
+
+            }
+        }
+
+        private void export(string tableName)
+        {
+            using (SQLiteConnection connection = createConnection())
+            {
+                List<string> columns = fetchColumns(connection, tableName);
+
+                string appFolder = Path.GetDirectoryName(Application.ExecutablePath);
+                string templateFileName = Path.Combine(appFolder, string.Format(@"resources\{0}.xlsx", tableName));
+                string exportFileName = string.Format("{0}.xlsx", tableName);
+
+                if (File.Exists(templateFileName))
+                {
+                    using (ExcelHelper helper = new ExcelHelper(templateFileName, exportFileName))
+                    {
+                        helper.Direction = ExcelHelper.DirectionType.TOP_TO_DOWN;
+                        helper.CurrentSheetName = "Sheet1";
+
+                        helper.InsertRange("header");
+                        CellRangeTemplate rowTemplate = helper.CreateCellRangeTemplate("row", columns);
+
+                        helper.InsertRange(rowTemplate, fetchData(connection, tableName, columns));
+
+                        helper.DeleteSheet("Templates");
+                    }
+
+                    MessageBox.Show("Exported!");
+                }
+                else
+                {
+                    MessageBox.Show(String.Format("Template xlsx not found ('{0}')!\nAutocreating it, please check it and retry!", templateFileName));
+                    string basicTempateFileName = Path.Combine(appFolder, @"resources\BasicTemplate.xlsx");
+                    using (ExcelHelper helper = new ExcelHelper(basicTempateFileName, templateFileName))
+                    {
+                        helper.Direction = ExcelHelper.DirectionType.LEFT_TO_RIGHT;
+                        helper.CurrentSheetName = "Templates";
+
+                        CellRangeTemplate template = helper.CreateCellRangeTemplate("header_row", new List<string>() { "header", "row" });
+
+                        var result = 
+                            from item in columns
+                            select new List<object>() { item, string.Format("<{0}>", item) };
+
+                        helper.InsertRange(template, result);
+
+                        var names = new string[] {"header", "row"};
+                        foreach (string name in names)
+                        {
+                            CellRangeRef range = helper.FindDefinedNameRange(name);
+                            
+                            //extending it to the length of the columns
+                            CellRef end = range.End;
+                            end.OffsetIt(new CellRef(0, columns.Count - 1));
+                            range.End = end;
+                            range.SheetName = "Templates";
+
+                            helper.SetDefinedNameRange(name, range);
+                        }
+
+                        helper.DeleteSheet("_Templates");
+                    }
+                }
+
+            }
+        }
+
+    }
+}
